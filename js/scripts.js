84acc2cec52bd358cd9cfdeae260a801355b5f9b
Daniel Arbiol <daniel@daniel.(none)>
2014-08-14 21:44:56 +0800

    Finished modal behavior.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,44 @@
 document.getElementById( 'show-modal' ).addEventListener( 'click', function () {
 	var modal = document.getElementById('addHero-modal');
 	var overlay = document.getElementById('overlay');
-	modal.style.display='block';
-	modal.style.visibility = 'visible';
-	document.getElementByTagName('body').style.overflow = 'hidden'
+	toggleModal( overlay, modal, 'active' );
 } );
 
 document.getElementById( 'dismiss-modal' ).addEventListener( 'click', function () {
-	var modal = document.getElementById('addHero-modal')
-	modal.style.display='none';
-	modal.style.visibility = 'hidden';
-	document.getElementByTagName('body').style.overflow = 'auto'
+	var modal = document.getElementById('addHero-modal');
+	var overlay = document.getElementById('overlay');
+	toggleModal( overlay, modal, 'inactive' );
 } );
+
+document.getElementById('overlay').addEventListener( 'click', function () {
+	var modal = document.getElementById('addHero-modal');
+	var overlay = document.getElementById('overlay');
+	toggleModal( overlay, modal, 'inactive' );
+} );
+
+function toggleVisibility ( element ) {
+
+	if( element.style.display === 'block' && element.style.visibility === 'visible' ) {
+		
+		element.style.display='none';
+		element.style.visibility = 'hidden';
+	
+	}else {
+		element.style.display='block';
+		element.style.visibility = 'visible';
+	}
+	return;
+}
+
+function toggleModal ( overlay, modal, state ) {
+	toggleVisibility( modal );
+	toggleVisibility( overlay );
+	// active = modal is active
+	if( state === 'active' ) {
+		document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+	}else {
+		document.getElementsByTagName('body')[0].style.overflow = 'scroll';
+	}		
+
+	return;
+}
